52a9169616a7d94b9c79bbe9c61e00545b382d5f
Tanveer Ahamed <tanveer1.mtm@gmail.com>
2023-02-05 11:54:51 +0530

    version 1.0 part 1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import './App.css';
-import About from './components/about/About';
-import Description from './components/description/Description';
-import Experience from './components/experience/Experience';
-import Certification from './components/certification/Certification';
-import Footer from './components/footer/Footer';
-// import { BsHeartHalf}  from 'react-icons/bs';
-import {FaHeartbeat} from 'react-icons/fa';
+import "./App.css";
+import About from "./components/about/About";
+import Description from "./components/description/Description";
+import Experience from "./components/experience/Experience";
+import Certification from "./components/certification/Certification";
+import Footer from "./components/footer/Footer";
+import { FaHeartbeat } from "react-icons/fa";
+import Main from "./components/main/Main";
 
 function App() {
-
-  return (
-    <div className="app-container">
-        <div className='left-space'></div>
-        <div className='main-container'>
-          <About/>
-          <Description/>
-          <Experience/>
-          <Certification/>
-          <Footer/>
-          <div className='end-quote'>Made with <span><FaHeartbeat/> </span></div>
-        </div>
-        <div className='right-space'></div>
-    </div>
-  );
+	return (
+		<Main />
+		// <div className='app-container'>
+		// 	<div className='left-space'></div>
+		// 	<div className='main-container'>
+		// 		<About />
+		// 		<Description />
+		// 		<Experience />
+		// 		<Certification />
+		// 		<Footer />
+		// 		<div className='end-quote'>
+		// 			Made with{" "}
+		// 			<span>
+		// 				<FaHeartbeat />{" "}
+		// 			</span>
+		// 		</div>
+		// 	</div>
+		// 	<div className='right-space'></div>
+		// </div>
+	);
 }
 
 export default App;
