42a97d8acac2d9b91ccf93d6986125d5f7aba03d
Tanveer Ahamed <tanveer1.mtm@gmail.com>
2023-02-08 12:33:20 +0530

    phame one release

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../../App.css";
 import UseMediaQuery from "../commonComponents/UseMediaQuery";
+import Resume from "../../assets/images/resume.svg";
+import TanveerAhamed from "../../assets/files/TANVEERAHAMED_07022023.pdf";
 
 const Header = () => {
 	const mobileView = UseMediaQuery("(max-width: 850px)");
@@ -9,13 +11,34 @@ const Header = () => {
 		<div className='genericContainer' style={header.container}>
 			<div style={{ ...header.logoContiner }}></div>
 
-			<div
+			<a
+				href={TanveerAhamed}
 				style={{
 					...header.hireMeContainer(mobileView),
 					fontSize: mobileView ? "1rem" : "1.2rem",
-				}}>
-				Resume
-			</div>
+				}}
+				download>
+				<div
+					style={{
+						display: "inline-block",
+						height: "100%",
+						marginTop: "5px",
+					}}>
+					<img
+						src={Resume}
+						style={{
+							width: mobileView ? "20px" : "30px",
+						}}
+					/>
+				</div>
+				<div
+					style={{
+						alignItems: "start",
+						height: "100%",
+					}}>
+					<p style={{ margin: "5px", color: "#00838f" }}>Resume</p>
+				</div>
+			</a>
 		</div>
 	);
 };
@@ -34,9 +57,10 @@ const header = {
 		position: "fixed",
 		color: "var(--clr-primary-0)",
 		right: isMobile ? "15px" : "15px",
-		border: "2px solid var(--clr-primary-0)",
-		borderRadius: "24px",
 		padding: "5px",
 		marginTop: "5px",
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
 	}),
 };
